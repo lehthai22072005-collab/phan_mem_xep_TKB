98e99d25a0e4a169e6f061b50d0ed840bd8dfc88
2k3hocviec <khoahoian03022003@gmail.com>
2026-06-22 13:18:32 +0700

    Thêm bảo vệ router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TeacherDashboard from "./pages/TeacherDashboard";
 import MinistryDashboard from "./pages/MinistryDashboard";
 import SysAdminDashboard from "./pages/SysAdminDashboard";
 import { Toaster } from "react-hot-toast";
+import { ProtectedRoute, PublicRoute } from "./components/ProtectedRoute";
 
 function App() {
     return (
@@ -26,14 +27,56 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     {/* Auth */}
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route
+                        path="/login"
+                        element={
+                            <PublicRoute>
+                                <Login />
+                            </PublicRoute>
+                        }
+                    />
+                    <Route
+                        path="/forgot-password"
+                        element={
+                            <PublicRoute>
+                                <ForgotPassword />
+                            </PublicRoute>
+                        }
+                    />
 
                     {/* Dashboards theo role */}
-                    <Route path="/student/*" element={<StudentDashboard />} />
-                    <Route path="/teacher/*" element={<TeacherDashboard />} />
-                    <Route path="/ministry/*" element={<MinistryDashboard />} />
-                    <Route path="/sysadmin/*" element={<SysAdminDashboard />} />
+                    <Route
+                        path="/student/*"
+                        element={
+                            <ProtectedRoute allowedRoles={["student"]}>
+                                <StudentDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/teacher/*"
+                        element={
+                            <ProtectedRoute allowedRoles={["teacher"]}>
+                                <TeacherDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/ministry/*"
+                        element={
+                            <ProtectedRoute allowedRoles={["ministry"]}>
+                                <MinistryDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/sysadmin/*"
+                        element={
+                            <ProtectedRoute allowedRoles={["sysadmin"]}>
+                                <SysAdminDashboard />
+                            </ProtectedRoute>
+                        }
+                    />
 
                     {/* Fallback */}
                     <Route path="/" element={<Navigate to="/login" replace />} />
